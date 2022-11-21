306d265d1f3b847d61f4aa952d424828e3e2e47c
sharko-m <m.pavlovets@mail.ru>
2022-11-21 13:20:32 +0300

    place211122

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$('.cities').slick({
+    dots: true,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    centerMode: true,
+    variableWidth: true
+  });
